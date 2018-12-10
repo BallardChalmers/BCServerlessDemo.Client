bcaeae42460c4100bdb1b4c3a434fe00232f3e4c
Kevin McDonnell <kevin@mcd79.com>
2018-12-10 16:20:18 +0000

    Replace real values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiEndpoint: 'http://localhost:7072/api',
-  tenant: 'bcserverlessdemo.onmicrosoft.com',
-  clientID: 'baf03ca9-a5d0-4862-9302-503603cea2af',
-  signUpSignInPolicy: 'B2C_1_SISU2',
-  b2cScopes: ['baf03ca9-a5d0-4862-9302-503603cea2af']
+  tenant: 'tenantToReplace',
+  clientID: 'clientIDToReplace',
+  signUpSignInPolicy: 'policyToReplace',
+  b2cScopes: ['clientIDToReplace']
 };
